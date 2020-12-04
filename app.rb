6be4d3ef5f682d565a4e70e4f05a38dfe12461ec
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-12-05 00:03:43 +0900

    Implement `GET /`

--- app.rb
+++ app.rb
@@ -1,1 +1,11 @@
 require 'sinatra'
+require_relative 'lib/poll'
+require_relative 'lib/vote'
+
+get '/' do
+  polls = [
+    Poll.new('好きな料理', ['肉じゃが', 'しょうが焼き', 'から揚げ']),
+    Poll.new('人気投票', ['おむすびけん', 'クックパッドたん']),
+  ]
+  erb :index, locals: { polls: polls }
+end
