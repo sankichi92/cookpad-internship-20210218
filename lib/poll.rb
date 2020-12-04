ca4b44da65933ddf00a99c6a852e73cf661c4d4b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-12-04 23:42:10 +0900

    Implement Poll#count_votes

--- lib/poll.rb
+++ lib/poll.rb
@@ -17,4 +17,18 @@ class Poll
 
     @votes.push(vote)
   end
+
+  def count_votes
+    result = {}
+
+    candidates.each do |candidate|
+      result[candidate] = 0
+    end
+
+    votes.each do |vote|
+      result[vote.candidate] += 1
+    end
+
+    result
+  end
 end
--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -27,4 +27,28 @@ RSpec.describe Poll do
       end
     end
   end
+
+
+  describe '#count_votes' do
+    it 'counts the votes and returns the result as a hash' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      poll.add_vote(Vote.new('Carol', 'Alice'))
+      poll.add_vote(Vote.new('Dave', 'Alice'))
+      poll.add_vote(Vote.new('Ellen', 'Bob'))
+
+      result = poll.count_votes
+
+      expect(result['Alice']).to eq 2
+      expect(result['Bob']).to eq 1
+
+      poll2 = Poll.new('Great Poll', ['Alice', 'Bob'])
+      poll2.add_vote(Vote.new('Carol', 'Bob'))
+      poll2.add_vote(Vote.new('Dave', 'Bob'))
+
+      result2 = poll2.count_votes
+
+      expect(result2['Alice']).to eq 0
+      expect(result2['Bob']).to eq 2
+    end
+  end
 end
