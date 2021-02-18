1f88446b5fb7a15ca4b4a1f4efbc0f83602ef8fa
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-18 14:13:39 +0900

    add test

--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -2,10 +2,14 @@ require_relative '../lib/poll.rb'
 
 RSpec.describe Poll do
   it 'has a title and candidates' do
-    poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+    poll = Poll.new('Great Poll', ['Alice', 'Bob'])
 
-    expect(poll.title).to eq 'Awesome Poll'
+    expect(poll.title).to eq 'Great Poll'
     expect(poll.candidates).to eq ['Alice', 'Bob']
 
+    poll = Poll.new('Awesome Poll', ['Alice', 'Charry'])
+
+    expect(poll.title).to eq 'Awesome Poll'
+    expect(poll.candidates).to eq ['Alice', 'Charry']
   end
 end
