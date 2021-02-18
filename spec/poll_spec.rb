44d14c402d2e2dfc65b844822ef5cf1c6ee09237
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-02-18 14:45:43 +0900

    move add_vote test to poll_spec

--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/vote'
 require_relative '../lib/poll'
 
 RSpec.describe Poll do
@@ -6,5 +7,25 @@ RSpec.describe Poll do
 
     expect(poll.title).to eq 'Awesome Poll'
     expect(poll.candidates).to eq ['Alice', 'Bob']
+  end
+  
+  describe '#add_vote' do
+    it 'saves the given vote' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      vote = Vote.new('Miyoshi', 'Alice')
+
+      poll.add_vote(vote)
+
+      expect(poll.votes).to include vote
+    end
+
+    context 'with a vote that has an invalid candidate' do
+      it 'raises InvalidCandidateError' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+        vote = Vote.new('Miyoshi', 'INVALID')
+
+        expect { poll.add_vote(vote) }.to raise_error Poll::InvalidCandidateError
+      end
+    end
   end
 end
--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -1,4 +1,3 @@
-require_relative '../lib/poll'
 require_relative '../lib/vote'
 
 RSpec.describe Vote do
@@ -7,25 +6,5 @@ RSpec.describe Vote do
 
     expect(vote.voter).to eq 'Miyoshi'
     expect(vote.candidate).to eq 'Alice'
-  end
-
-  describe '#add_vote' do
-    it 'saves the given vote' do
-      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
-      vote = Vote.new('Miyoshi', 'Alice')
-
-      poll.add_vote(vote)
-
-      expect(poll.votes).to include vote
-    end
-  end
-
-  context 'with a vote that has an invalid candidate' do
-    it 'raises InvalidCandidateError' do
-      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
-      vote = Vote.new('Miyoshi', 'INVALID')
-
-      expect { poll.add_vote(vote) }.to raise_error Poll::InvalidCandidateError
-    end
   end
 end
