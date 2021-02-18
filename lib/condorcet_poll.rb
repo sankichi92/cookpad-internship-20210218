48ccc1fce7fcaaf6c85297c6324cfbf50d4fdeb8
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-02-18 16:46:38 +0900

    add CondorcetPoll test

---
+++ lib/condorcet_poll.rb
@@ -0,0 +1,35 @@
+require 'set'
+
+class CondorcetPoll
+  class InvalidCandidateError < StandardError
+  end
+  class MultipleVoteError < StandardError
+  end
+  class OverdueVoteError < StandardError
+  end
+
+  attr_reader :title, :candidates, :votes, :closing
+
+  def initialize(title, candidates, closing)
+    @title = title
+    @candidates = candidates
+    @closing = closing
+    @votes = []
+  end
+
+  def add_vote(vote)
+    unless (vote.candidates.to_set ^ @candidates.to_set).length == 0 then
+      raise InvalidCandidateError.new(vote.candidates)
+    end
+
+    if @votes.map {|element| element.voter }.include?(vote.voter) then
+      raise MultipleVoteError.new(vote)
+    end
+
+    if DateTime.now() > closing then
+      raise OverdueVoteError.new(closing)
+    end
+
+    @votes.push(vote)
+  end
+end
---
+++ spec/condorcet_poll_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../lib/ranked_vote'
+require_relative '../lib/condorcet_poll'
+require 'date'
+
+RSpec.describe CondorcetPoll do
+  it 'has a title and candidates' do
+    poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.new(2020, 2, 18, 14, 57, 00, 0.125))
+
+    expect(poll.title).to eq 'Awesome Poll'
+    expect(poll.candidates).to eq ['Alice', 'Bob']
+    expect(poll.closing).to eq DateTime.new(2020, 2, 18, 14, 57, 00, 0.125)
+  end
+  
+  describe '#add_vote' do
+    it 'saves the given vote' do
+      poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.now + 10)
+      vote = RankedVote.new('Miyoshi', ['Alice', 'Bob'])
+
+      poll.add_vote(vote)
+
+      expect(poll.votes).to include vote
+    end
+
+    context 'with a vote that has an invalid candidate' do
+      it 'raises InvalidCandidateError' do
+        poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.now + 10)
+        vote = RankedVote.new('Miyoshi', ['INVALID', 'Bob'])
+
+        expect { poll.add_vote(vote) }.to raise_error CondorcetPoll::InvalidCandidateError
+      end
+
+      it 'raises InvalidCandidateError' do
+        poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.now + 10)
+        vote = RankedVote.new('Miyoshi', ['Bob', 'Bob'])
+
+        expect { poll.add_vote(vote) }.to raise_error CondorcetPoll::InvalidCandidateError
+      end
+    end
+
+    context 'with a vote that has an invalid voter' do
+      it 'raises MultipleVoteError' do
+        poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.now + 10)
+        vote = RankedVote.new('Miyoshi', ['Alice', 'Bob'])
+
+        poll.add_vote(vote)
+        expect { poll.add_vote(vote) }.to raise_error CondorcetPoll::MultipleVoteError
+      end
+    end
+
+    context 'with a vote that is overdue' do
+      it 'raises MultipleVoteError' do
+        poll = CondorcetPoll.new('Awesome Poll', ['Alice', 'Bob'], DateTime.now - 10)
+        vote = RankedVote.new('Miyoshi', ['Alice', 'Bob'])
+
+        expect { poll.add_vote(vote) }.to raise_error CondorcetPoll::OverdueVoteError
+      end
+    end
+  end
+end
