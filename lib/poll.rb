04fad4a064e8475c2f286dd3e38370c5e6fea17f
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-18 14:11:12 +0900

    skelton

---
+++ lib/poll.rb
@@ -0,0 +1,14 @@
+class Poll
+  def initialize(title, candidates)
+    @title = title
+    @candidates = candidates
+  end
+
+  def title
+    @title
+  end
+
+  def candidates
+    @candidates
+  end
+end
---
+++ spec/poll_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/poll.rb'
+
+RSpec.describe Poll do
+  it 'has a title and candidates' do
+    poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+
+    expect(poll.title).to eq 'Awesome Poll'
+    expect(poll.candidates).to eq ['Alice', 'Bob']
+
+  end
+end
