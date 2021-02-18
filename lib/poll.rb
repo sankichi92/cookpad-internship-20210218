4a129ee96f046b19306bb0e06417f3c47564c802
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-02-18 14:24:15 +0900

    add add_vote poll test and impl

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,8 +1,13 @@
 class Poll
-  attr_reader :title, :candidates
+  attr_reader :title, :candidates, :votes
 
   def initialize(title, candidates)
     @title = title
     @candidates = candidates
+    @votes = []
+  end
+
+  def add_vote(vote)
+    @votes.push(vote)
   end
 end
--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Vote do
     expect(vote.voter).to eq 'Miyoshi'
     expect(vote.candidate).to eq 'Alice'
   end
+
+  describe '#add_vote' do
+    it 'saves the given vote' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      vote = Vote.new('Miyoshi', 'Alcie')
+
+      poll.add_vote(vote)
+
+      expect(poll.votes).to eq [vote]
+    end
+  end
 end
