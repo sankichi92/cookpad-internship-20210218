c5a722a02473fbe787a4a719cc2130139c36f22f
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-18 16:14:44 +0900

    return 400 when invalid candidate

--- app.rb
+++ app.rb
@@ -17,6 +17,22 @@ get '/polls/:id' do
   index = params['id'].to_i
   poll = $polls[index]
   halt 404, '投票が見つかりませんでした' if poll.nil?
-
   erb :poll, locals: { index: index, poll: poll }
 end
+
+post '/polls/:id/votes' do
+  index = params['id'].to_i
+  poll = $polls[index]
+  halt 404, '投票が見つかりませんでした' if poll.nil?
+  voter = params[:voter]
+  candidate = params[:candidate]
+
+  begin
+    poll.add_vote(Vote.new(voter, candidate))
+  rescue => e
+    halt 400, '無効な投票先です'
+  end
+
+  redirect to('/polls/' + index.to_s), 303
+end
+
