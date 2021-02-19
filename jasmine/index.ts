a26cb12fe7289a9b744cd596d0c499e69dc7a8ab
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-19 18:15:09 +0900

    hmac

--- jasmine/index.ts
+++ jasmine/index.ts
@@ -1,6 +1,6 @@
 import * as Index from '../ts/index';
 
-describe("buf2str", () => {
+describe('buf2str', () => {
   it('convert 0xff, 0xff', () => {
     const target = new Uint8Array([0xff, 0xff]);
     expect(Index.buf2str(target.buffer)).toEqual('ffff');
@@ -15,7 +15,7 @@ describe("buf2str", () => {
   });
 });
 
-describe("str2buf", () => {
+describe('str2buf', () => {
   it('convert ffff', () => {
     const target = new Uint8Array([0xff, 0xff]);
     const buf = Index.str2buf('ffff');
@@ -27,18 +27,31 @@ describe("str2buf", () => {
     expect(new Uint8Array(buf)).toEqual(target);
   });
   it('cannot convert ffff', () => {
-    expect(() => Index.str2buf('fff')).toThrow(new Error('hash string has odd length'));
+    expect(() => Index.str2buf('fff')).toThrow(
+      new Error('hash string has odd length')
+    );
   });
 });
 
-
-describe("generate_salt_and_encrypted_pass", () => {
- it('generate pubkey', (done: DoneFn) => {
-   Index.generate_salt_and_encrypted_pass("password").then(pair => {
-     expect(pair.password.length).toEqual(64);
-     expect(pair.pubkey.alg).toEqual('RSA-OAEP-256');
-     done();
-   });
- });
+describe('generate_salt_and_encrypted_pass', () => {
+  it('generate pubkey', (done: DoneFn) => {
+    Index.generate_salt_and_encrypted_pass('password').then((pair) => {
+      expect(pair.password.length).toEqual(64);
+      expect(pair.pubkey.alg).toEqual('RSA-OAEP-256');
+      done();
+    });
+  });
 });
 
+describe('hmac', () => {
+  it('hmac', (done: DoneFn) => {
+    const pass = '7768617420646f2079612077616e7420666f72206e6f7468696e673f';
+    const key = '4a656665';
+    const result =
+      '5bdcc146bf60754e6a042426089575c75a003f089d2739839dec58b964ec3843';
+    Index.hmac(Index.str2buf(pass), Index.str2buf(key)).then((dig) => {
+      expect(Index.buf2str(dig)).toEqual(result);
+    });
+    done();
+  });
+});
--- ts/index.ts
+++ ts/index.ts
@@ -1,44 +1,132 @@
 export interface KeyPair {
-  password: string,
-  pubkey: JsonWebKey
+  password: string;
+  pubkey: JsonWebKey;
 }
 
 export function buf2str(buf: ArrayBuffer): string {
   const view = new Uint8Array(buf);
-  return Array.prototype.map.call(view, byte => ("00" + byte.toString(16)).slice(-2)).join("")
+  return Array.prototype.map
+    .call(view, (byte) => ('00' + byte.toString(16)).slice(-2))
+    .join('');
 }
 
 export function str2buf(s: string): ArrayBuffer {
-  if (s.length % 2 != 0) throw new Error("hash string has odd length");
+  if (s.length % 2 != 0) throw new Error('hash string has odd length');
   const buf = new Uint8Array(s.length / 2);
-  for (let i = 0; i < s.length/2; ++i) {
-    buf[i] = parseInt(s.slice(i*2, i*2+2), 16);
+  for (let i = 0; i < s.length / 2; ++i) {
+    buf[i] = parseInt(s.slice(i * 2, i * 2 + 2), 16);
   }
-  return buf.buffer
+  return buf.buffer;
 }
 
-export function generate_salt_and_encrypted_pass(password: string): Promise<KeyPair> {
-  const encode = new TextEncoder();
-  const raw_pass = encode.encode(password);
+const keyConfig = {
+  name: 'RSA-OAEP',
+  modulusLength: 4096,
+  publicExponent: new Uint8Array([1, 0, 1]),
+  hash: 'SHA-256',
+};
+const encryptConfig = {
+  name: 'RSA-OAEP',
+};
+
+export function digest_pass(
+  pass: string,
+  key: CryptoKey
+): Promise<ArrayBuffer> {
+  const raw_pass = new TextEncoder().encode(pass);
   return new Promise((resolve, reject) => {
-    const config = {
-      name: "RSA-OAEP",
-      modulusLength: 4096,
-      publicExponent: new Uint8Array([1,0,1]),
-      hash: "SHA-256",
-    };
-    crypto.subtle.generateKey(config, true, ["encrypt", "decrypt"]).then(key => {
-      const config = {
-        name: "RSA-OAEP",
-      };
-      crypto.subtle.encrypt(config, key.publicKey, raw_pass).then(encrypted => {;
-        crypto.subtle.exportKey("jwk", key.publicKey).then(pubkey => {
-          crypto.subtle.digest("SHA-256", encrypted).then(password_digest => {
-            resolve({password: buf2str(password_digest), pubkey});
-          });
+    crypto.subtle
+      .encrypt(encryptConfig, key, raw_pass)
+      .then((encrypted) => {
+        crypto.subtle
+          .digest('SHA-256', encrypted)
+          .then((password_digest) => {
+            resolve(password_digest);
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+export function generate_salt_and_encrypted_pass(
+  password: string
+): Promise<KeyPair> {
+  return new Promise((resolve, reject) => {
+    crypto.subtle
+      .generateKey(keyConfig, true, ['encrypt', 'decrypt'])
+      .then((key) => {
+        crypto.subtle
+          .exportKey('jwk', key.publicKey)
+          .then((pubkey) => {
+            digest_pass(password, key.publicKey).then((pass) =>
+              resolve({
+                password: buf2str(pass),
+                pubkey: pubkey,
+              })
+            );
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+export function hmac(
+  msg_buf: ArrayBuffer,
+  key_buf: ArrayBuffer
+): Promise<ArrayBuffer> {
+  const msg = new Uint8Array(msg_buf);
+  const key = new Uint8Array(key_buf);
+  const ipad_k_m = new Uint8Array(64 + msg.byteLength);
+  for (let i = 0; i < 64; ++i) {
+    ipad_k_m[i] = 0x36;
+  }
+  for (let i = 0; i < key.byteLength; ++i) {
+    ipad_k_m[i] ^= key[i];
+  }
+  for (let i = 0; i < msg.byteLength; ++i) {
+    ipad_k_m[i + 64] = msg[i];
+  }
+  return new Promise((resolve, reject) => {
+    crypto.subtle
+      .digest('SHA-256', ipad_k_m)
+      .then((i_dig_buf) => {
+        const i_dig = new Uint8Array(i_dig_buf);
+        const opad_k_m = new Uint8Array(64 + i_dig.byteLength);
+        for (let i = 0; i < 64; ++i) {
+          opad_k_m[i] = 0x5c;
+        }
+        for (let i = 0; i < key.byteLength; ++i) {
+          opad_k_m[i] ^= key[i];
+        }
+        for (let i = 0; i < i_dig.byteLength; ++i) {
+          opad_k_m[i + 64] = i_dig[i];
+        }
+        crypto.subtle.digest('SHA-256', opad_k_m).then(resolve).catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+export function hmac_pass(
+  pass: string,
+  key: JsonWebKey,
+  token: string
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    crypto.subtle
+      .importKey('jwk', key, 'RSA-OAEP', false, ['encrypt'])
+      .then((cryptoKey) => {
+        digest_pass(pass, cryptoKey).then((pass) => {
+          const raw_token = str2buf(token);
+          hmac(pass, raw_token)
+            .then((dig) => {
+              resolve(buf2str(dig));
+            })
+            .catch(reject);
         });
-      });
-    })
-    .catch(e => reject(e))
-  })
+      })
+      .catch(reject);
+  });
 }
