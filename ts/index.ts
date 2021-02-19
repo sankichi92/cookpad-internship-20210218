6c495ffc50d466d57b0a6cbe98e82a9d6e1397a2
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-19 15:23:38 +0900

    encrypter

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,21 @@
-console.log('hello World');
+function generate_salt_and_encrypted_pass(password: string) {
+  const encode = new TextEncoder();
+  const raw_pass = encode.encode(password);
+  new Promise((resolve, reject) => {
+    const config = {
+      name: "RSA-OAEP",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([1,0,1]),
+      hash: "SHA-256",
+    };
+    crypto.subtle.generateKey(config, true, ["encrypt"]).then(key => {
+      const config = {
+        name: "RSA-OAEP",
+      };
+      const encrypted = crypto.subtle.encrypt(config, key.publicKey, raw_pass);
+      const pubkey = crypto.subtle.exportKey("jwk", key.publicKey);
+      resolve([encrypted, pubkey]);
+    })
+    .catch(e => reject(e))
+  })
+}
