2f2eccf92d45e26f691b5ed41cecebc42b9f8513
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-19 16:01:23 +0900

    impl buf2array

--- jasmine/index.ts
+++ jasmine/index.ts
@@ -1,6 +1,19 @@
- describe('console.log', () => {
-   it('should show window object', () => {
-     console.log(typeof window);
-     // => 'object'
+import * as Index from '../ts/index';
+
+describe("buf2str", () => {
+  it('convert 0xff, 0xff', () => {
+    const target = new Uint8Array([0xff, 0xff]);
+    expect(Index.buf2str(target)).toEqual('ffff');
+  });
+  it('convert 0xff, 0xff', () => {
+    const target = new Uint8Array([0x01, 0x02]);
+    expect(Index.buf2str(target)).toEqual('0102');
+  });
+  it('convert empty', () => {
+    const target = new Uint8Array([]);
+    expect(Index.buf2str(target)).toEqual('');
+  });
+});
+
    });
  });
--- ts/index.ts
+++ ts/index.ts
@@ -1,20 +1,32 @@
-function generate_salt_and_encrypted_pass(password: string) {
+export interface KeyPair {
+  password: string,
+  pubkey: JsonWebKey
+}
+
+export function buf2str(buf: ArrayBuffer): string {
+  const view = new Uint8Array(buf);
+  return Array.prototype.map.call(view, byte => ("00" + byte.toString(16)).slice(-2)).join("")
+}
+
+export function generate_salt_and_encrypted_pass(password: string): Promise<KeyPair> {
   const encode = new TextEncoder();
   const raw_pass = encode.encode(password);
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const config = {
       name: "RSA-OAEP",
       modulusLength: 4096,
       publicExponent: new Uint8Array([1,0,1]),
       hash: "SHA-256",
     };
-    crypto.subtle.generateKey(config, true, ["encrypt"]).then(key => {
+    crypto.subtle.generateKey(config, true, ["encrypt", "decrypt"]).then(key => {
       const config = {
         name: "RSA-OAEP",
       };
-      const encrypted = crypto.subtle.encrypt(config, key.publicKey, raw_pass);
-      const pubkey = crypto.subtle.exportKey("jwk", key.publicKey);
-      resolve([encrypted, pubkey]);
+      crypto.subtle.encrypt(config, key.publicKey, raw_pass).then(encrypted => {;
+        crypto.subtle.exportKey("jwk", key.publicKey).then(pubkey => {
+          resolve({password: buf2str(encrypted), pubkey});
+        });
+      });
     })
     .catch(e => reject(e))
   })
