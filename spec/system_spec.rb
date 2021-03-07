cc7985471b4aa8a71eb68047b90256aeb013f866
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-07 20:00:33 +0900

    test duplicated signup

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -10,7 +10,46 @@ RSpec.describe 'System' do
     $polls = []
   end
 
-  it 'GET /', js: true do
-    visit 'localhost:4567/'
+  context 'with no login' do
+    it 'GET /', js: true do
+      visit 'localhost:4567/'
+      expect(page).to have_content('ログイン')
+      expect(page).to_not have_content('追加')
+      expect(page).to_not have_content('登録')
+    end
+
+    it 'GET /polls/0', js: true do
+      visit 'localhost:4567/polls/0'
+      expect(page).to_not have_content('投票する')
+    end
+
+    it 'GET /login' do
+      visit 'localhost:4567/login'
+      expect(page).to have_content('登録')
+    end
+  end
+
+  context 'signup' do
+    it 'GET /signup' do
+      visit 'localhost:4567/signup'
+      fill_in 'signup-username', with: 'namachan'
+      fill_in 'signup-password', with: 'the-password'
+      find_by_id('signup-confirm').click
+      expect(current_path).to eq('/')
+      expect(page).to have_content('登録')
+      expect(page).to have_content('追加')
+    end
+
+    context 'duplicated signup' do
+      it 'GET /signup' do
+        visit 'localhost:4567/signup'
+        fill_in 'signup-username', with: 'namachan'
+        fill_in 'signup-password', with: 'the-password'
+        find_by_id('signup-confirm').click
+        msg = accept_confirm {}
+        expect(msg).to eq ('既に登録されています')
+        expect(current_path).to eq('/signup')
+      end
+    end
   end
 end
